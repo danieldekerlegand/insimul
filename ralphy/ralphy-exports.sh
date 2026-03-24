376c494e30845643ed23fea6908a13ab64f1f04a
Daniel DeKerlegand <dldekerl@gmail.com>
2026-03-24 06:13:50 -0500

    complete more unity tasks

--- ralphy/ralphy-exports.sh
+++ ralphy/ralphy-exports.sh
@@ -1,3 +1,1 @@
-ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-godot.json && \
-ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-unity.json && \
-ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-unreal.json
+ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-godot.json
