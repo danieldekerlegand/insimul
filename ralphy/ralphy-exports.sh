c771c5fc7d9b3601d637f70244af22f4e1965d38
Daniel DeKerlegand <dldekerl@gmail.com>
2026-03-24 05:03:22 -0500

    update ralphy tasklists

--- ralphy/ralphy-exports.sh
+++ ralphy/ralphy-exports.sh
@@ -1,4 +1,3 @@
-ralphy --parallel --max-parallel 4 --json ralphy/ralphy-export-foundation.json && \
 ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-godot.json && \
 ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-unity.json && \
 ralphy --parallel --max-parallel 3 --json ralphy/ralphy-export-unreal.json
